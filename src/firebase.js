507b8a8f4613ad5147d26c41e7c3dcae6f365ea4
Mark Langhamer <langhamerm@gmail.com>
2021-03-16 07:10:29 -0500

    sda

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,17 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 // firebase init
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
-}
+  apiKey: "AIzaSyA_ZQDwxoatmd7i56PZHxzve90_Y6m0FGM",
+  authDomain: "fcc-social.firebaseapp.com",
+  databaseURL: "https://fcc-social.firebaseio.com",
+  projectId: "fcc-social",
+  storageBucket: "fcc-social.appspot.com",
+  messagingSenderId: "307314994355",
+  appId: "1:307314994355:web:ac9a6c17162c8aeb7454c6",
+  measurementId: "G-3JX7DY5RE2"
+};
 firebase.initializeApp(firebaseConfig)
 
 // utils
